863989fab232548639b554c3ea2d2b484663daeb
Nathan-Binkley <nbink2@gmail.com>
2019-05-14 15:57:18 -0400

    Initial upload

---
+++ fun_sorts.py
@@ -0,0 +1,26 @@
+#Fun Sorts.py
+#Idea is to take a bunch of funny sorts found on r/programmerhumor and make them real
+
+O(1) Optimization Sort: Delete the whole list, an empty list is sorted list.
+
+O(-1) Sort: Fuck off, I'm not sorting your list.
+
+StalinSort: You iterate down the list of elements checking if they're in order. An element which is out of order is eliminated. At the end you have a sorted list.
+
+GenghisKhanSort: delete all elements except for the first, repopulate the list with successors of the first element.
+
+HitlerSort: pick the value that you like, declare it as the highest, and then delete all other values.
+
+AmishSort: Turn off computer, then you won't even need sorting.
+
+Communist Sort: Wait for the list to sort itself. Act upset when it doesn't happen until a tyrannical dictatorship shows up and forces the list into sorted order.
+
+Capitalist Sort: The first 3 elements in the list remain in their position, every iteration through they subtract 1 from all of the other elements and add it to themselves, until the rest of the list is destroyed and they are the only ones left.
+
+Thanos Sort: Randomly delete half the elements in the list over and over until the list happens to be sorted.
+
+TrumpSort O(0): the array is always sorted. Anyone who says otherwise is fake news.
+
+LiberalSort: each element is declared to be out of order for moral reasons and committing moral crimes, elements have 1 subtracted from them at semi-random. The sort never actually ends as the narrative needs to keep going to maintain political power.
+
+Republic Sort: Look at the first 5 elements. Choose the highest one no matter how much lower that is than other elements in the list past that. Declare that the new top value, the list never gets sorted because it would upset the newly declared top element.
