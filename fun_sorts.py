f77cee9ab48d020381a6aef1cca0bf8d71e08529
Nathan-Binkley <nbink2@gmail.com>
2019-05-21 15:35:37 -0400

    Adventure Capitalist (no copyright)

--- fun_sorts.py
+++ fun_sorts.py
@@ -28,7 +28,6 @@ def no_sort(nums:list) -> list:
 #
 def stalin_sort(nums:list) -> list:
     newList = nums[:]
-    print(newList)
     nextList = []
     while(not check(newList)):
         for i in range(len(newList)-1):
@@ -80,7 +79,7 @@ def AmishSort(nums:list) -> list:
     print("Why are you here? We're Amish. We don't use computers")
     print("Well now we're gonna sort anyway")
     print("Sorting")
-    for i in range(20):
+    for i in range(5):
         print(".")
         time.sleep(1)
 
@@ -89,6 +88,37 @@ def AmishSort(nums:list) -> list:
 
 # Capitalist Sort: The first 3 elements in the list remain in their position, every iteration through they subtract 1 from all of the other elements and add it to themselves, until the rest of the list is destroyed and they are the only ones left.
 #
+def capitalist(nums:list) -> list:
+    newList = nums[:]
+    first = newList[0]
+    second = newList[1]
+    third = newList[2]
+    done = False
+    while (not done):
+        for i in range(len(newList)):
+            if i <= 2:
+                pass
+            elif newList[i] == 0:
+                newList[i] = None
+            else:
+                newList[i] -= 1
+                if i % 3 == 0:
+                    first += 1
+                elif i % 3 == 1:
+                    second += 1
+                elif i% 3 == 2:
+                    third += 1
+        nextList = []
+        for i in newList:
+            if i != None:
+                nextList.append(i)
+        newList = nextList[:]
+        newList[0] = first
+        newList[1] = second
+        newList[2] = third
+        if len(newList) <= 3:
+            return newList
+
 
 # Thanos Sort: Randomly delete half the elements in the list over and over until the list happens to be sorted.
 #
@@ -157,13 +187,19 @@ print("Amish Sort: ")
 best = AmishSort(init)
 print("Amish sort doesn't sort. It just turns off your PC (if it actually worked the way I wanted it to).")
 print("Here's the outcome: " + str(best))
+print()
+print()
 
 # Communist Sort: Wait for the list to sort itself. Act upset when it doesn't happen until a tyrannical dictatorship shows up and forces the list into sorted order.
 #
 
 # Capitalist Sort: The first 3 elements in the list remain in their position, every iteration through they subtract 1 from all of the other elements and add it to themselves, until the rest of the list is destroyed and they are the only ones left.
 #
-
+print("Capitalist Sort: ")
+best = capitalist(init)
+print("This is the outcome of capitalist sort: " + str(best))
+print()
+print()
 # Thanos Sort: Randomly delete half the elements in the list over and over until the list happens to be sorted.
 #
 
